422fbe42fb3b98c116d15a36077c6106e4c4afb1
Minnu Prakash <minnumsr24@gmail.com>
2025-09-10 21:35:17 +0530

    Motor and Bit Manipulation

---
+++ AVR/Blink_with_BIT_Manipulation/Blink_with_BIT_Manipulation/main.c
@@ -0,0 +1,26 @@
+/*
+ * Blink_with_BIT_Manipulation.c
+ *
+ * Created: 10-09-2025 20:21:05
+ * Author : MinnuDiary
+ */ 
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include<util/delay.h>
+
+
+int main(void)
+{
+   DDRB= DDRB|(1<<DDB5); // bit made as output to connect LED
+   
+   
+    while (1) 
+    {
+		PORTB=PORTB|(1<<PORTB5); // LED CONNECTED TO PB5
+		_delay_ms(1000);
+		PORTB=PORTB&~ (1<<PORTB5);  // CAN BE WRITTEN AS PORTB&=~(1<<PORT5)
+		_delay_ms(1000);
+    }
+}
+
---
+++ AVR/Motor_Driving/Motor_Driving/main.c
@@ -0,0 +1,35 @@
+/*
+ * Motor_Driving.c
+ *
+ * Created: 10-09-2025 21:10:33
+ * Author : MinnuDiary
+ */ 
+
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include<util/delay.h>
+
+
+int main(void)
+{
+   DDRB= DDRB|(1<<DDB5); // bit made as output to connect LED
+   DDRB= DDRB|(1<<DDB4);
+   
+    while (1) 
+    {
+		PORTB=PORTB|(1<<PORTB5); // 1  0 
+		PORTB=PORTB&~ (1<<PORTB4); 
+		_delay_ms(10000);
+		PORTB=PORTB&~ (1<<PORTB5);   // OFF
+		PORTB=PORTB&~ (1<<PORTB4);
+		_delay_ms(1000);	
+		PORTB=PORTB|(1<<PORTB4); //0 1
+		PORTB=PORTB&~ (1<<PORTB5); 
+		_delay_ms(10000);
+		
+		
+    }
+}
+
+
